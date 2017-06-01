db85ef740b4b75f45b12951763ffc3033e666472
Brian <brian.parry@dell.com>
2017-06-01 14:12:56 -0400

    Add x-ignore-validate option to request validation
    * This option allows specified parameters to bypass validation.

--- test/test-operation.js
+++ test/test-operation.js
@@ -407,6 +407,23 @@ describe('Operation', function () {
           ]);
         });
 
+        it('should not return an error for skipped parameters', function () {
+          var operation = swaggerApiRelativeRefs.getOperation('/pet/{petId}/uploadImage', 'post');
+
+          operation.getParameters()[0].definition['x-ignore-validate'] = true;
+          var results = operation.validateRequest({
+            url: '/v2/pet/notANumber/uploadImage',
+            headers: {
+              'content-type': 'multipart/form-data'
+            },
+            body: {},
+            files: {}
+          });
+
+          assert.equal(results.errors.length, 0);
+          assert.equal(results.warnings.length, 0);
+        });
+
         it('should not return an error for valid parameters', function () {
           var operation = swaggerApiRelativeRefs.getOperation('/pet/{petId}', 'post');
           var results = operation.validateRequest({
