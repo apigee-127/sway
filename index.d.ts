2a9227798436c9a6fe8cac934afbd0df01b98f0e
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2018-07-19 14:28:18 -0700

    TypeScript definitions (#35)
    * include `index.d.ts` file to `package.json`
    
    * tag type
    
    * Options
    
    * SwaggerObject
    
    * more types
    
    * LiveResponse
    
    * ValidateResponse
    
    * SchemaObject
    
    * Schema
    
    * format
    
    * format is not readonly
    
    * examples
    
    * schema in Response
    
    * optional warnings
    
    * "x-ms-parameterized-host"
    
    * hostTemplate
    
    * host
    
    * schemes
    
    * basePath
    
    * remove 4.0
    
    * node.js 8
    
    * node latest
    
    * gulp 4.0 => gulp 3.0

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,184 @@
-export declare function create(options : Options) : Promise<SwaggerApi>
-
-declare type Options = {
-    defintion: Object | string,
-    jsonRefs?: Object,
-    customValidatiors?: ValidatorCallback[]
+interface SwaggerObject {
+    "x-ms-paths"?: PathsObject
+    paths?: PathsObject
+    definitions?: DefinitionsObject
+    "x-ms-parameterized-host"?: XMsParameterizedHost
+    consumes?: string[]
+    produces?: string[]
+    parameters?: ParametersDefinitionsObject
+    responses?: ResponsesDefinitionsObject
+    readonly documents?: any
 }
 
-declare type ValidatorCallback = (api : SwaggerApi) => ValidationResults
+interface PathsObject {
+    [name: string]: PathItemObject
+}
 
-declare class SwaggerApi{
+interface DefinitionsObject {
+    [name: string]: SchemaObject
+}
+
+interface ParameterObject {
+    name: string
+    in: string
+    schema?: SchemaObject
+    required?: boolean
+    items?: SchemaObject
+    type?: DataType
+}
+
+interface ParametersDefinitionsObject {
+    [name: string]: ParameterObject
+}
+
+interface ResponsesDefinitionsObject {
+    [name: string]: ResponseObject
+}
+
+interface PathItemObject extends PathItemObjectMethods {
+    parameters?: ParameterObject[]
+}
+
+interface SchemaObject {
+    type?: DataType
+    items?: SchemaObject
+    properties?: JsonSchemaProperties
+    additionalProperties?: SchemaObject|boolean
+    "x-nullable"?: boolean
+    in?: string
+    oneOf?: SchemaObject[]
+    $ref?: string
+    required?: string[]|false
+    schema?: SchemaObject
+    allOf?: SchemaObject[]
+    description?: string
+    discriminator?: string
+    "x-ms-discriminator-value"?: string
+    enum?: string[]
+    "x-ms-azure-resource"?: boolean
+    anyOf?: SchemaObject[]
+}
+
+type DataType = "integer"|"number"|"string"|"boolean"|"null"|"file"|"object"|"array"
+
+interface ResponseObject {
+    schema?: SchemaObject
+}
+
+type PathItemObjectMethods = {
+    [m in Methods]?: OperationObject
+}
+
+/**
+ * JSON Schema "properties"
+ */
+interface JsonSchemaProperties {
+    [name: string]: SchemaObject
+}
+
+type Methods = "get"|"put"|"post"|"delete"|"options"|"head"|"patch"
+
+interface OperationObject {
+    operationId?: string
+    parameters?: ParameterObject[]
+    consumes?: string[]
+    produces?: string[]
+    responses?: ResponsesObject
+}
+
+interface ResponsesObject {
+    default?: ResponseObject
+    [name: string]: ResponseObject|undefined
+}
+
+declare interface Options {
+    definition: SwaggerObject | string
+    jsonRefs?: {
+        readonly relativeBase: any
+    }
+    customValidatiors?: ValidatorCallback[]
+    readonly isPathCaseSensitive?: boolean
+}
+
+declare type ValidationResults = {
+    errors: ValidationEntry[],
+    warnings?: ValidationEntry[]
+}
+
+declare class Path {
+    path: string
+    regexp: RegExp
+    api: SwaggerApi
+
+    definition: Object
+    definitionFullyResolved: Object
+    operationObjects: Operation[]
+    parameterObjects: Parameter[]
+
+    pathToDefinition: string[]
+    ptr: string
+
+    getOperation(method : string): Operation[]
+    getOperations(): Operation[]
+    getOperationsByTag(tag: any): Operation[]
+    getParameters(): Parameter[]
+}
+
+declare interface Schema {
+    readonly example: any
+    format: any
+}
+
+declare class Parameter{
+    definition: Object
+    definitionFullyResolved: Object
+    operationObject: Operation
+    pathObject: Path
+    pathToDefinition: string[]
+    ptr: string
+    schema: Schema
+    name: any
+    format: any
+    required: any
+    in: any
+    "x-ms-skip-url-encoding": any
+    type: any
+
+    getSample() : Object
+    getValue(request : Request): ParameterValue
+}
+
+declare interface Operation {
+    readonly operationId?: any
+    method: string
+    pathObject: Path
+    provider?: any
+    readonly responses?: ResponsesObject
+    "x-ms-examples": any
+    readonly consumes: string[]
+    readonly produces: any
+    definition: Object
+    definitionFullyResolved: Object
+    pathToDefinition: string[]
+    parameterObjects: Parameter[]
+    ptr: string
+    securityDefinitions: Object
+
+    validateRequest(request : Request) : ValidationResults
+    validateResponse(response : LiveResponse) : ValidationResults
+    getParameters() : Parameter[]
+    getResponses() : Response[]
+    getResponse(statusCode ?: number | string) : Response
+    getParameter(name : string, location ?: string) : Parameter
+    getSecurity() : Object//Security
+}
+
+declare interface XMsParameterizedHost {
+    parameters: ParameterObject[]
+    readonly hostTemplate: any
+}
+
+declare interface SwaggerApi{
     customValidators: ValidatorCallback[]
     definition: Object
     definitionRemotesResolved: Object
@@ -18,6 +188,14 @@ declare class SwaggerApi{
     options: Object
     references: Object
     version: string
+    readonly info: {
+        readonly version: string
+        readonly title: any
+    }
+    readonly "x-ms-parameterized-host": XMsParameterizedHost
+    readonly host: any
+    readonly schemes: string[]
+    readonly basePath: any
 
     getOperation(pathOrRequest : string | Request, method ?: string): Operation
     getOperations(path ?: string): Operation[]
@@ -28,35 +206,20 @@ declare class SwaggerApi{
     validate(): ValidationResults
 }
 
-//TODO confirm this is all any of them use
+export declare function create(options : Options) : Promise<SwaggerApi>
+
+//
+
+declare type ValidatorCallback = (api : SwaggerApi) => ValidationResults
+
+interface ParsedUrlQuery {
+    [key: string]: any
+}
+
 declare interface Request {
-    [index: string]: any
-    url: string
-}
-
-
-declare class Operation{
-    definition: Object
-    definitionFullyResolved: Object
-    method: string
-    pathObject: Path
-    pathToDefinition: string[]
-    parameterObjects: Parameter[]
-    ptr: string
-    securityDefinitions: Object
-    
-    getParameter(name : string, location ?: string) : Parameter
-    getParameters() : Parameter[]
-    getResponse(statusCode ?: number | string) : Response
-    getResponses() : Response[]
-    getSecurity() : Object//Security
-    validateRequest(request : Request) : ValidationResults
-    validateResponse(response : Response) : ValidationResults
-}
-
-declare type ValidationResults = {
-    errors: ValidationEntry[],
-    warnings: ValidationEntry[]
+    query?: ParsedUrlQuery
+    readonly url: string
+    readonly method: string
 }
 
 declare type ValidationEntry = {
@@ -71,47 +234,22 @@ declare type ValidationEntry = {
     schemaId: string
 }
 
-declare class Response{
+declare interface LiveResponse {
+}
+
+declare interface Response {
     definition: Object
     definitionFullyResolved: Object
     operationObject: Operation
     pathToDefinition: string[]
     ptr: string
     statusCode: string
+    readonly examples: any
+    readonly schema: any
 
     getExample(mimeType: string): string
     getSample(): Object
-    validateResponse(response: Response): ValidationResults
-}
-
-declare class Parameter{
-    definition: Object
-    definitionFullyResolved: Object
-    operationObject: Operation
-    pathObject: Path
-    pathToDefinition: string[]
-    ptr: string
-    schema: Object
-
-    getSample() : Object
-    getValue(request : Request): ParameterValue
-}
-
-declare class Path{
-    api: SwaggerApi
-    definition: Object
-    definitionFullyResolved: Object
-    operationObjects: Operation[]
-    parameterObjects: Parameter[]
-    path: string
-    pathToDefinition: string[]
-    ptr: string
-    regexp: RegExp
-
-    getOperation(method : string): Operation[]
-    getOperations(): Operation[]
-    getOperationsByTag(tag): Operation[]
-    getParameters(): Parameter[]
+    validateResponse(response: LiveResponse): ValidationResults
 }
 
 declare class ParameterValue{
