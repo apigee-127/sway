1785bbfcfda771697db323243656a5f2c64e28f4
Daniel Golub <danielg6666@gmail.com>
2023-05-07 16:48:11 +0300

    chore: stable point of the fork (all tasks working)

--- test/test-issues.js
+++ test/test-issues.js
@@ -57,9 +57,9 @@ describe('issues', function () {
       })
       .catch(function (err) {
         var errorMessages = [
-          'Cannot read property \'consumes\' of null', // Node.js
+          'Cannot read properties of null (reading \'consumes\')', // Node.js
           'null is not an object (evaluating \'definitionFullyResolved.consumes\')' // PhantomJS (browser)
-        ];
+        ]
 
         assert.ok(errorMessages.indexOf(err.message) > -1);
       })
